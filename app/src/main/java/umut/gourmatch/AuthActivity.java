20442eb4263aa5f898defafac604b3bb31992b5f
mtiedeman <mtiedeman831@utexas.edu>
2016-07-19 20:03:46 -0500

    Design and auth additions
    Added colors and strings; made initial auth xml

--- app/src/main/java/umut/gourmatch/AuthActivity.java
+++ app/src/main/java/umut/gourmatch/AuthActivity.java
@@ -1,13 +1,42 @@
 package umut.gourmatch;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AuthActivity extends AppCompatActivity {
+
+    private static final String TAG = "AuthActivity.java";
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
         setContentView(R.layout.activity_auth);
+    }
+
+    public void login(View view) {
+        //Check email and password combo
+
     }
 }
