ea5acdbf21c5b83e27b535f3e647d6aa29b3aca1
mtiedeman <mtiedeman831@utexas.edu>
2016-08-08 14:37:53 -0500

    Added listing object

---
+++ app/src/main/java/umut/gourmatch/ListingObj.java
@@ -0,0 +1,12 @@
+package umut.gourmatch;
+
+/**
+ * Created by Megan on 8/3/2016.
+ */
+
+/*
+Should contain all the information for a listing
+May be used to pass information
+ */
+public class ListingObj {
+}
