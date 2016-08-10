e51fecd1a5b1086cee7f6268ba6f26d825a08992
mtiedeman <mtiedeman831@utexas.edu>
2016-08-09 22:27:01 -0500

    Finished initial listings set up and listings object

--- app/src/main/java/umut/gourmatch/ListingObj.java
+++ app/src/main/java/umut/gourmatch/ListingObj.java
@@ -1,5 +1,20 @@
 package umut.gourmatch;
 
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.content.Context;
+
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
 /**
  * Created by Megan on 8/3/2016.
  */
@@ -9,4 +24,121 @@ Should contain all the information for a listing
 May be used to pass information
  */
 public class ListingObj {
+    private final int OWN;
+    private final int GUEST;
+    public String listingID;
+    public int color;
+    private boolean empty = true;
+    private HashMap<String, Object> list;
+    private Context context;
+
+    private String[] infoMini;
+
+    public ListingObj(String id, boolean owner, Context c) {
+        context = c;
+        OWN = ContextCompat.getColor(context, R.color.listing_owned);
+        GUEST = ContextCompat.getColor(context, R.color.listing_owned);
+        if(owner) {
+            color = OWN;
+        } else {
+            color = GUEST;
+        }
+        listingID = id;
+        infoMini = context.getResources().getStringArray(R.array.short_listing);
+        list = new HashMap<String, Object>();
+        Thread a = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DatabaseReference mD = FirebaseDatabase.getInstance().getReference();
+                mD.child("listings").child(listingID).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild("uid")) {
+                            empty = false;
+                            collectData(dataSnapshot);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+        a.run();
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //end
+    }
+
+    private void collectData(DataSnapshot dataSnapshot) {
+        Iterable<DataSnapshot> iterable = dataSnapshot.getChildren();
+        Iterator<DataSnapshot> it = iterable.iterator();
+        while(it.hasNext()) {
+            DataSnapshot curr = it.next();
+            String title = curr.getKey();
+            Object val = curr.getValue();
+            list.put(title, val);
+        }
+        notify();
+    }
+
+    public ArrayList<String[]> getInfoMini() {
+        ArrayList<String[]> total = new ArrayList<>();
+        for(String title : infoMini) {
+            //0:Title, 1:Value, 2:bold, 3:italicized, 4:size level (small, med, large)
+            String[] curr = new String[5];
+            curr[0] = title;
+            int id = context.getResources().getIdentifier(title, "array", context.getPackageName());
+            String[] dbPull = context.getResources().getStringArray(id);
+            String[] pulled = new String[dbPull.length];
+            for(int i = 0; i < dbPull.length; i++) {
+                pulled[i] = (String) list.get(dbPull[0]);
+            }
+            String sep = ", ";
+            String val = "";
+            if(title.contains("time") || title.contains("Time")) {
+                sep = ":";
+            }
+            if(title.contains("date") || title.contains("Date")) {
+                String month = "";
+                String day = "";
+                String year = "";
+                for(int i = 0; i < dbPull.length; i++) {
+                    if(dbPull[i].contains("month") || dbPull[i].contains("Month")) {
+                        month = new DateFormatSymbols().getMonths()[Integer.parseInt(pulled[i])];
+                    }
+                    else if(dbPull[i].contains("year") || dbPull[i].contains("Year")) {
+                        year = pulled[i];
+                    } else {
+                        day = pulled[i];
+                    }
+                }
+                val = month + day + sep + year;
+            } else  {
+                for (int i = 0; i < pulled.length; i++) {
+                    val += pulled[i];
+                    if(i+1 != pulled.length) {
+                        val += sep;
+                    }
+                }
+            }
+            curr[1] = val;
+            int idSet = context.getResources().getIdentifier(title+"_settings", "array", context.getPackageName());
+            String[] set = context.getResources().getStringArray(idSet);
+            curr[2] = set[0];
+            curr[3] = set[1];
+            curr[4] = set[2];
+            total.add(curr);
+        }
+        return total;
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
 }
--- app/src/main/java/umut/gourmatch/Listings.java
+++ app/src/main/java/umut/gourmatch/Listings.java
@@ -1,12 +1,29 @@
 package umut.gourmatch;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 /**
@@ -18,16 +35,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Listings extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private ArrayList<ListingObj> list;
+    private DatabaseReference mDatabase;
 
     public Listings() {
         // Required empty public constructor
@@ -45,8 +56,8 @@ public class Listings extends Fragment {
     public static Listings newInstance(String param1, String param2) {
         Listings fragment = new Listings();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,17 +65,112 @@ public class Listings extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listings, container, false);
+        View view = inflater.inflate(R.layout.fragment_listings, container, false);
+        final ScrollView sv = (ScrollView) view.findViewById(R.id.scrollView);
+//        int ownColor = ContextCompat.getColor(getContext(), R.color.listing_owned);
+//        int guestColor = ContextCompat.getColor(getContext(), R.color.listing_guest);
+        String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("listings").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        if(dataSnapshot.hasChild("owner")) {
+                            DataSnapshot ownedListings = dataSnapshot.child("owner");
+                            Iterable<DataSnapshot> children = ownedListings.getChildren();
+                            for(DataSnapshot x : children) {
+                                //May or may not work
+                                ListingObj curr = new ListingObj((String) x.getValue(), true, getContext());
+
+                                list.add(curr);
+                            }
+                        }
+                        if(dataSnapshot.hasChild("guest")) {
+                            DataSnapshot ownedListings = dataSnapshot.child("guest");
+                            Iterable<DataSnapshot> children = ownedListings.getChildren();
+                            for(DataSnapshot x : children) {
+                                //May or may not work
+                                ListingObj curr = new ListingObj((String) x.getValue(), false, getContext());
+                                list.add(curr);
+                            }
+                        }
+                        for(final ListingObj obj : list) {
+                            //0:Title, 1:Value, 2:bold, 3:italicized, 4:size level (small, med, large)
+                            ArrayList<String[]> info = obj.getInfoMini();
+                            LinearLayout lv = new LinearLayout(getContext());
+                            lv.setBackgroundColor(obj.color);
+                            lv.setOrientation(LinearLayout.VERTICAL);
+                            for(String[] text : info) {
+                                LinearLayout lh = new LinearLayout(getContext());
+                                lh.setOrientation(LinearLayout.HORIZONTAL);
+                                TextView title = new TextView(getContext());
+                                title.setText(text[0] + ": ");
+                                title.setTypeface(null, Typeface.BOLD);
+                                TextView val = new TextView(getContext());
+                                val.setText(text[1]);
+                                boolean bold = text[2].equalsIgnoreCase("true");
+                                boolean it = text[3].equalsIgnoreCase("true");
+                                if(bold && it) {
+                                    val.setTypeface(null, Typeface.BOLD_ITALIC);
+                                } else if(bold) {
+                                    val.setTypeface(null, Typeface.BOLD);
+                                } else if(it) {
+                                    val.setTypeface(null, Typeface.ITALIC);
+                                }
+                                if(text[4].equalsIgnoreCase("small")) {
+                                    val.setTextSize(getResources().getDimension(R.dimen.text_size_small));
+                                    title.setTextSize(getResources().getDimension(R.dimen.text_size_small));
+                                } else if(text[4].equalsIgnoreCase("large")){
+                                    val.setTextSize(getResources().getDimension(R.dimen.text_size_large));
+                                    title.setTextSize(getResources().getDimension(R.dimen.text_size_large));
+                                } else {
+                                    val.setTextSize(getResources().getDimension(R.dimen.text_size_medium));
+                                    title.setTextSize(getResources().getDimension(R.dimen.text_size_medium));
+                                }
+                                lh.addView(title);
+                                lh.addView(val);
+                                lv.addView(lh);
+                            }
+                            lv.setClickable(true);
+                            lv.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    ///Uncomment when a listing viewer is created and modify accordingly
+//                                    Intent intent = new Intent(view.getContext().getApplicationContext(), ListingActivity.class);
+//                                    intent.putExtra("LID", obj.listingID);
+//                                    startActivity(intent);
+                                }
+                            });
+                            //Would add them in reverse, but keep plus at bottom
+                            //sv.addView(lv, 0);
+                        }
+
+//                        else {
+//                            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+//                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                            startActivity(intent);
+//                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //Throw up a toast
+                    }
+                }
+        );
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
